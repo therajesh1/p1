feb229262de56ccb0e17e4f1e5322b762fe29866
therajesh1 <therajeshchheda@gmail.com>
2024-10-31 08:46:00 +0530

    fg

--- p1/settings.py
+++ p1/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'p2',
+    'p2.apps.P2Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
